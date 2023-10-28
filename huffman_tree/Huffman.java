bf59dcf8a02c5c6994ac90d016bd474e90e7280a
de-mark <data.engineer.mark@gmail.com>
2023-10-28 11:07:33 -0700

    Huffman - Added frequency table

--- huffman_tree/Huffman.java
+++ huffman_tree/Huffman.java
@@ -1,5 +1,66 @@
 package huffman_tree;
 
 public class Huffman {
+    private int[] freqTable;
+    private Tree huffTree;
 
+    public Huffman() {
+        freqTable = new int[27];
+        clearFreqTable();
+    }
+
+    private void clearFreqTable() {
+        for (int i = 0; i < freqTable.length; i++) {
+            freqTable[i] = 0;
+        }
+    }
+
+    private int getIndexForChar(char c) {
+        int asciiVal = (int) c;
+        int idx;
+
+        if (asciiVal == 32) {
+            idx = 26;
+        } else {
+            idx = asciiVal - (int) 'a';
+        }
+
+        return idx;
+    }
+
+    private char getCharForIndex(int idx) {
+        if (idx == 26) {
+            return ' ';
+        } else {
+            return (char) (idx + (int) 'a');
+        }
+    }
+
+    public void encodeMessage(String messageToEncode) {
+        clearFreqTable();
+        messageToEncode = messageToEncode.replaceAll("\\p{Punct}", "")
+                .replaceAll("\\d", "")
+                .toLowerCase()
+                .trim()
+                .replaceAll(" +", " ");
+
+        for (int m = 0; m < messageToEncode.length(); m++) {
+            char c = (char) messageToEncode.charAt(m);
+            int idx = getIndexForChar(c);
+
+            freqTable[idx]++;
+        }
+    }
+
+    public void displayFrequencyTable() {
+        for (int i = 0; i < 27; i++) {
+            if (freqTable[i] != 0) {
+                System.out.printf("[ CHAR: %c | FREQ: %d]\n", getCharForIndex(i), freqTable[i]);
+            }
+        }
+    }
+
+    // public Tree decodeMessage() {
+
+    // }
 }
--- huffman_tree/Testing.java
+++ huffman_tree/Testing.java
@@ -2,51 +2,47 @@ package huffman_tree;
 
 public class Testing {
     public static void main(String[] args) {
-        // PriorityQueue pq = new PriorityQueue(25);
-        // pq.enqueue(43);
-        // pq.enqueue(333);
-        // pq.enqueue(345);
-        // pq.enqueue(45);
-        // pq.enqueue(3);
-        // pq.enqueue(400);
-        // System.out.println(pq.dequeue());
-        // System.out.println(pq.dequeue());
-        // System.out.println(pq.dequeue());
-        // System.out.println(pq.dequeue());
-        // System.out.println(pq.dequeue());
-        // System.out.println(pq.dequeue());
+        // TESTING MINHEAP
 
-        MinHeapPQ pq = new MinHeapPQ(25);
+        // MinHeapPQ pq = new MinHeapPQ(25);
 
-        Tree a = new Tree();
-        a.insert('a', 5);
-        a.insert('e', 2);
-        a.insert('d', 7);
-        System.out.println("[A]");
-        a.display();
+        // Tree a = new Tree();
+        // a.insert('a', 5);
+        // a.insert('e', 2);
+        // a.insert('d', 7);
+        // System.out.println("[A]");
+        // a.display();
 
-        Tree b = new Tree();
-        b.insert('b', 10);
-        b.insert('q', 12);
-        b.insert('z', 1);
-        System.out.println("[A]");
-        b.display();
+        // Tree b = new Tree();
+        // b.insert('b', 10);
+        // b.insert('q', 12);
+        // b.insert('z', 1);
+        // System.out.println("[A]");
+        // b.display();
 
-        Tree c = new Tree();
-        c.insert('c', 2);
-        c.insert('x', 10);
-        c.insert('z', 1);
-        System.out.println("[A]");
-        c.display();
+        // Tree c = new Tree();
+        // c.insert('c', 2);
+        // c.insert('x', 10);
+        // c.insert('z', 1);
+        // System.out.println("[A]");
+        // c.display();
 
-        pq.enqueue(a);
-        pq.enqueue(b);
-        pq.enqueue(c);
+        // pq.enqueue(a);
+        // pq.enqueue(b);
+        // pq.enqueue(c);
 
-        System.out.println("[PQ HEAD]");
-        pq.peek().display();
-        System.out.println("[PQ REMOVED TOP]");
-        pq.dequeue();
-        pq.peek().display();
+        // System.out.println("[PQ HEAD]");
+        // pq.peek().display();
+        // System.out.println("[PQ REMOVED TOP]");
+        // pq.dequeue();
+        // pq.peek().display();
+        //
+
+        // TESTING HUFFMAN
+
+        Huffman hm = new Huffman();
+
+        hm.encodeMessage("a 72823 suzie says it's 78ea8sy!!!!");
+        hm.displayFrequencyTable();
     }
 }
